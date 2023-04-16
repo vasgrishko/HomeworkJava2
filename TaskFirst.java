a4cbfdf81bb429fc7f5bfbd97c6757931a634de0
vas <vas.grishko@yandex.ru>
2023-04-16 21:45:18 +0300

    Написали программу для 1-й задачи

--- TaskFirst.java
+++ TaskFirst.java
@@ -1,5 +1,26 @@
 package Homework.HomeworkJava2;
 
+
+
 public class TaskFirst {
-    
+    public static void main(String[] args) {
+        String str = "585";
+        boolean result = DefinePalindrome(str);
+        if (result) {
+            System.out.println(str + " палиндром");
+        } else {
+            System.out.println(str + " не палиндром");
+        }   
+    }
+    public static boolean DefinePalindrome(String str){
+        int strLength = str.length();
+        int countOfWays = strLength / 2;
+        for (int i = 0; i < countOfWays; i++) {
+            if (str.charAt(i) != str.charAt(strLength - 1 - i)) {
+                return false;
+            }
+        }
+        return true;       
+    }
 }
+
